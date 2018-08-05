5c532dd0d9b06438bafd5b22b01406a86370dd49
ahmed Oumezzine <ahmedoumezzine@outlook.fr>
2018-08-06 00:18:18 +0100

    fix instanceperrequest

--- DI_Pattern_Autofac.Web/App_Start/AutofacConfig.cs
+++ DI_Pattern_Autofac.Web/App_Start/AutofacConfig.cs
@@ -25,9 +25,11 @@ namespace DI_Pattern_Autofac.Web.App_Start
             //builder.RegisterModule(new DataModule(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\ahmed\source\repos\DI_Pattern_Autofac\DI_Pattern_Autofac.Web\App_Data\Database1.mdf;Integrated Security=True"));
             //  builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().SingleInstance();
 
+#pragma warning disable CS0618 // Type or member is obsolete
             builder.RegisterType<UnitOfWork>().As<IUnitOfWork>()
            .WithParameter("dbContextFactory", new DbContextFactory())
-           .InstancePerHttpRequest();
+           .InstancePerRequest();
+#pragma warning restore CS0618 // Type or member is obsolete
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
